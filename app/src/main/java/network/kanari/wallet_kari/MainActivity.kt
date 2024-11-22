9f8609233757f4a82b7afd496ad2f18a50f7b5c1
jamesatomc <jiraphonwansai2002@gmail.com>
2024-11-22 23:51:39 +0700

    add Biometric Login

--- app/src/main/java/network/kanari/wallet_kari/MainActivity.kt
+++ app/src/main/java/network/kanari/wallet_kari/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.compose.rememberNavController
@@ -72,10 +73,24 @@ class MainActivity : FragmentActivity() {
                     Box(
                         modifier = Modifier
                             .fillMaxSize()
-                            .background(Color.Black),
+                            .background(Color.DarkGray)
+                            .padding(16.dp),
                         contentAlignment = Alignment.Center
                     ) {
-                        CircularProgressIndicator()
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            CircularProgressIndicator(
+                                color = Color.White,
+                                strokeWidth = 8.dp,
+                                modifier = Modifier.size(64.dp)
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            Button(onClick = { biometricPrompt.authenticate(promptInfo) }) {
+                                Text(text = "Authenticate")
+                            }
+                        }
                     }
                 }
             }
